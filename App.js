deb79d6fcbedba53123a49e5120395f32112002a
notArodnt <pierre.kowalski.pro@gmail.com>
2023-12-02 15:29:25 +0100

    Tabs created

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Chat from './tabs/Chat'
+import Profile from './tabs/Profile'
+import Search from './tabs/Search'
+import { AppRegistry } from 'react-native';
+console.log(createBottomTabNavigator)
+const Tab = createBottomTabNavigator()
 
+AppRegistry.registerComponent('main', () => App)
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Chat" component={Chat} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
