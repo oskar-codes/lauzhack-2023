c6b543662985dad389138b151e950285c608a150
Oskar <oskar.codes@gmail.com>
2023-12-02 18:48:05 +0100

    Fixed la fonction de julie

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,23 +1,23 @@
 import OpenAI from 'openai';
 
+console.log('contentScript.js loaded');
 
-
-const openai = new OpenAI({
-  apiKey: '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', // defaults to process.env["OPENAI_API_KEY"]
+(() => {
+  const openai = new OpenAI({
+    apiKey: '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', // defaults to process.env["OPENAI_API_KEY"]
+  });
+  
+  async function test() {
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: 'Say this is a test' }],
+      model: 'gpt-4.0-turbo',
+    });
+    console.log(chatCompletion);
+  }
 });
 
-async function test() {
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: 'Say this is a test' }],
-    model: 'gpt-4.0-turbo',
-  });
-  console.log(chatCompletion);
-}
-
 function findTermsAndConditionsLinksInPage() {
-  return document.getElementsByTagName('a').filter((a) => a.innerText.toLowerCase().includes('terms') || a.innerText.toLowerCase().includes('conditions')).map((a) => a.href);
+  return Array.from(document.getElementsByTagName('a')).filter((a) => a.innerText.toLowerCase().includes('terms') || a.innerText.toLowerCase().includes('conditions')).map((a) => a.href);
 }
 
 console.log(findTermsAndConditionsLinksInPage());
-
-console.log(document.documentElement.outerHTML);
