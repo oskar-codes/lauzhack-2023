ca79337a2fcbc34406cd6213846f8a1b56fdbf97
notArodnt <pierre.kowalski.pro@gmail.com>
2023-12-02 23:14:43 +0100

    prompts folder

--- src/extractTextFromPage.js
+++ src/extractTextFromPage.js
@@ -1,5 +1,200 @@
-const targetURL = 'https://openai.com/policies/terms-of-use';
+const targetURL = 'https://auth0.openai.com/u/signup/identifier?state=hKFo2SA4Rm56eEZmcWZ3ay1VVi1LUy1DTGw0LW0wbFRqcmhtd6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIGRZV1pnelY1Y2hkSk42Tjl3VlVLbTVVZnBBdHZWaGNzo2NpZNkgVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEc';
+const page = `<div class=\"oai-wrapper\">
+<header class=\"oai-header\">
+  <svg viewBox=\"140 140 520 520\" xmlns=\"http://www.w3.org/2000/svg\">
+    <path d=\"m617.24 354a126.36 126.36 0 0 0 -10.86-103.79 127.8 127.8 0 0 0 -137.65-61.32 126.36 126.36 0 0 0 -95.31-42.49 127.81 127.81 0 0 0 -121.92 88.49 126.4 126.4 0 0 0 -84.5 61.3 127.82 127.82 0 0 0 15.72 149.86 126.36 126.36 0 0 0 10.86 103.79 127.81 127.81 0 0 0 137.65 61.32 126.36 126.36 0 0 0 95.31 42.49 127.81 127.81 0 0 0 121.96-88.54 126.4 126.4 0 0 0 84.5-61.3 127.82 127.82 0 0 0 -15.76-149.81zm-190.66 266.49a94.79 94.79 0 0 1 -60.85-22c.77-.42 2.12-1.16 3-1.7l101-58.34a16.42 16.42 0 0 0 8.3-14.37v-142.39l42.69 24.65a1.52 1.52 0 0 1 .83 1.17v117.92a95.18 95.18 0 0 1 -94.97 95.06zm-204.24-87.23a94.74 94.74 0 0 1 -11.34-63.7c.75.45 2.06 1.25 3 1.79l101 58.34a16.44 16.44 0 0 0 16.59 0l123.31-71.2v49.3a1.53 1.53 0 0 1 -.61 1.31l-102.1 58.95a95.16 95.16 0 0 1 -129.85-34.79zm-26.57-220.49a94.71 94.71 0 0 1 49.48-41.68c0 .87-.05 2.41-.05 3.48v116.68a16.41 16.41 0 0 0 8.29 14.36l123.31 71.19-42.69 24.65a1.53 1.53 0 0 1 -1.44.13l-102.11-59a95.16 95.16 0 0 1 -34.79-129.81zm350.74 81.62-123.31-71.2 42.69-24.64a1.53 1.53 0 0 1 1.44-.13l102.11 58.95a95.08 95.08 0 0 1 -14.69 171.55c0-.88 0-2.42 0-3.49v-116.68a16.4 16.4 0 0 0 -8.24-14.36zm42.49-63.95c-.75-.46-2.06-1.25-3-1.79l-101-58.34a16.46 16.46 0 0 0 -16.59 0l-123.31 71.2v-49.3a1.53 1.53 0 0 1 .61-1.31l102.1-58.9a95.07 95.07 0 0 1 141.19 98.44zm-267.11 87.87-42.7-24.65a1.52 1.52 0 0 1 -.83-1.17v-117.92a95.07 95.07 0 0 1 155.9-73c-.77.42-2.11 1.16-3 1.7l-101 58.34a16.41 16.41 0 0 0 -8.3 14.36zm23.19-50 54.92-31.72 54.92 31.7v63.42l-54.92 31.7-54.92-31.7z\"></path>
+  </svg>
+</header><main class=\"_widget c1a2c4d90\">
+<section class=\"cf93c37e0 _prompt-box-outer c0ccc2e41\">
+<div class=\"cbeab5b12 cc55737ca\">
+<div class=\"c29e7b4da\">
+  <header class=\"c8a98d04c c38e24e0e\">
+    <div title=\"OpenAI\" id=\"custom-prompt-logo\" style=\"width: auto !important; height: 60px !important; position: static !important; margin: auto !important; padding: 0 !important; background-color: transparent !important; background-position: center !important; background-size: contain !important; background-repeat: no-repeat !important\"></div>
+  
+    <img class=\"c6da7d6a3 cc4d8d0a1\" id=\"prompt-logo-center\" src=\"https://cdn.openai.com/API/images/openai-logomark.png\" alt=\"OpenAI\">
+  
+    
+      <h1 class=\"c3f16bbb8 c15aaf151\">Create your account</h1>
+    
+  
+    <div class=\"c72fb3216 c23d69759\">
+      <p class=\"c192ddc3e cfcd4ae5f\"> </p>
+    </div>
+  </header>
 
+  <div class=\"cdf8ec7e1 c546fd776\">
+    
+  
+    <div class=\"c8822552e cfa4fc908\">
+      <div class=\"c1a1eca04\">
+        
+      
+        <form method=\"POST\" class=\"c03b1c121 _form-signup-id\" data-form-primary=\"true\">
+          <input type=\"hidden\" name=\"state\" value=\"hKFo2SA4Rm56eEZmcWZ3ay1VVi1LUy1DTGw0LW0wbFRqcmhtd6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIGRZV1pnelY1Y2hkSk42Tjl3VlVLbTVVZnBBdHZWaGNzo2NpZNkgVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEc\">
+        
+          
+        
+          <div class=\"c8822552e cfa4fc908\">
+            <div class=\"c1a1eca04\">
+              
+            
+              
+                <div class=\"input-wrapper _input-wrapper\">
+                  <div class=\"ccaa5e013 cc158b895 text cb5308bf6 cb5f4882f\" data-action-text=\"\" data-alternate-action-text=\"\">
+                    <label class=\"c752698fd no-js c4d626e37 c940de3ac\" for=\"email\">
+                      Email address
+                    </label>
+                  
+                    <input class=\"input cb8b8ac5f cbf3fe8f1\" inputmode=\"email\" name=\"email\" id=\"email\" type=\"text\" value=\"\" required=\"\" autocomplete=\"username\" autocapitalize=\"none\" spellcheck=\"false\" autofocus=\"\">
+                  
+                    <div class=\"c752698fd js-required c4d626e37 c940de3ac\" data-dynamic-label-for=\"email\" aria-hidden=\"true\">
+                      Email address
+                    </div>
+                  </div>
+                
+                  
+                </div>
+              
+            
+              
+            </div>
+          </div>
+        
+          <div id=\"custom-field-1\"></div>
+        
+          
+        
+          
+        
+          <div class=\"c3fc7dc62\">
+            
+              <button type=\"submit\" name=\"action\" value=\"default\" class=\"c64e877fc c55256a89 c283fdb19 c26d7a986 _button-signup-id\" data-action-button-primary=\"true\">Continue</button>
+            
+          </div>
+        </form>
+      </div>
+    </div>
+  
+    <div class=\"ulp-alternate-action  _alternate-action \">
+      
+    
+      <p class=\"c192ddc3e cfcd4ae5f c87f0cc8e\">Already have an account?
+        <a class=\"c72dde48d c36964393\" href=\"/u/login/identifier?state=hKFo2SA4Rm56eEZmcWZ3ay1VVi1LUy1DTGw0LW0wbFRqcmhtd6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIGRZV1pnelY1Y2hkSk42Tjl3VlVLbTVVZnBBdHZWaGNzo2NpZNkgVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEc\" aria-label=\"\">Log in</a>
+      </p>
+    
+      
+    </div>
+  
+    
+      <div class=\"c715d18fd cdf15c565\">
+        <span>Or</span>
+      </div>
+    
+      <div class=\"c4d7fa6ff cc0c2fa7c\">
+        
+      
+        
+          <form method=\"post\" data-provider=\"windowslive\" class=\"c0403f43c c8c6ba0db cc2a75ce7\" data-form-secondary=\"true\">
+            <input type=\"hidden\" name=\"state\" value=\"hKFo2SA4Rm56eEZmcWZ3ay1VVi1LUy1DTGw0LW0wbFRqcmhtd6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIGRZV1pnelY1Y2hkSk42Tjl3VlVLbTVVZnBBdHZWaGNzo2NpZNkgVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEc\">
+          
+            <input type=\"hidden\" name=\"connection\" value=\"windowslive\">
+          
+            <button type=\"submit\" class=\"c829a69f7 c0263e40f ce1e3dbfe\" data-provider=\"windowslive\" data-action-button-secondary=\"true\">
+              
+                <span class=\"cb3201224 c668e3005\" data-provider=\"windowslive\"></span>
+              
+            
+              <span class=\"cfe625857\">Continue with Microsoft Account</span>
+            </button>
+          </form>
+        
+          <form method=\"post\" data-provider=\"google\" class=\"c0403f43c c8c6ba0db c6c29d59c\" data-form-secondary=\"true\">
+            <input type=\"hidden\" name=\"state\" value=\"hKFo2SA4Rm56eEZmcWZ3ay1VVi1LUy1DTGw0LW0wbFRqcmhtd6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIGRZV1pnelY1Y2hkSk42Tjl3VlVLbTVVZnBBdHZWaGNzo2NpZNkgVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEc\">
+          
+            <input type=\"hidden\" name=\"connection\" value=\"google-oauth2\">
+          
+            <button type=\"submit\" class=\"c829a69f7 c0263e40f c743f0739\" data-provider=\"google\" data-action-button-secondary=\"true\">
+              
+                <span class=\"cb3201224 c668e3005\" data-provider=\"google\"></span>
+              
+            
+              <span class=\"cfe625857\">Continue with Google</span>
+            </button>
+          </form>
+        
+          <form method=\"post\" data-provider=\"apple\" class=\"c0403f43c c8c6ba0db c242abf60\" data-form-secondary=\"true\">
+            <input type=\"hidden\" name=\"state\" value=\"hKFo2SA4Rm56eEZmcWZ3ay1VVi1LUy1DTGw0LW0wbFRqcmhtd6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIGRZV1pnelY1Y2hkSk42Tjl3VlVLbTVVZnBBdHZWaGNzo2NpZNkgVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEc\">
+          
+            <input type=\"hidden\" name=\"connection\" value=\"apple\">
+          
+            <button type=\"submit\" class=\"c829a69f7 c0263e40f c917feb83\" data-provider=\"apple\" data-action-button-secondary=\"true\">
+              
+                <span class=\"cb3201224 c668e3005\" data-provider=\"apple\"></span>
+              
+            
+              <span class=\"cfe625857\">Continue with Apple</span>
+            </button>
+          </form>
+        
+      
+        
+      </div>
+    
+  </div>
+</div>
+</div>
+
+
+</section>
+</main>
+<script type=\"text/javascript\" async=\"\" src=\"https://chat.openai.com/ces/v1/auth.js\"></script><script id=\"client-scripts\">
+!function(){var e,t,n,w,A,E,C,S,r=((e={})
+.exports=function(n,a){var r={};function u(e,t)
+{if(e.classList)return e.classList.add(t);var n=e.
+  className.split(\" \");-1===n.indexOf(t)&
+  &(n.push(t),e.className=n.join(\" \"))
+}function i(e,t,n,r){return e.addEventLi
+  stener(t,n,r)}function o(e){return\"s
+  tring\"==typeof e}function c(e,t){retur
+</script>
+<footer class=\"oai-footer\">
+  <a href=\"https://openai.com/policies/terms-of-use\" target=\"_blank\">Terms of use</a>
+  |
+  <a href=\"https://openai.com/policies/privacy-policy\" target=\"_blank\">Privacy policy</a>
+</footer>
+</div>
+
+<script type=\"text/javascript\">
+(() => {
+  var isProd=window.location.origin.includes(\"auth0.openai.com\"),clientEventsServiceDomain=isProd?\"chat.openai.com/ces\":\"client-events.apps.openai.org\";
+  var authJsPath = \`https://\${clientEventsServiceDomain}/v1/auth.js\`
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error(\"Segment snippet included twice.\");else{analytics.invoked=!0;analytics.methods=[\"trackSubmit\",\"trackClick\",\"trackLink\",\"trackForm\",\"pageview\",\"identify\",\"reset\",\"group\",\"track\",\"ready\",\"alias\",\"debug\",\"page\",\"once\",\"off\",\"on\",\"addSourceMiddleware\",\"addIntegrationMiddleware\",\"setAnonymousId\",\"addDestinationMiddleware\"];analytics.factory=function(e){return function(){if(window.analytics.initialized)return window.analytics[e].apply(window.analytics,arguments);var i=Array.prototype.slice.call(arguments);i.unshift(e);analytics.push(i);return analytics}};for(var i=0;i<analytics.methods.length;i++){var key=analytics.methods[i];analytics[key]=analytics.factory(key)}analytics.load=function(key,i){var t=document.createElement(\"script\");t.type=\"text/javascript\";t.async=!0;t.src=authJsPath;var n=document.getElementsByTagName(\"script\")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=i};;
+  analytics.load(\"nu3JDoANZbyeGreR5Ekcm5dX2WtPPnnq\");
+  analytics.page(\"Auth0 page: signup-id\");
+  }}();
+})()
+
+function updateHeader(text) {
+    const $h1 = document.querySelector('main header > h1');
+    if ($h1) {
+        $h1.innerText = text;
+    }
+}
+
+    updateHeader('Create your account');
+
+
+
+
+</script>
+
+
+
+<script src=\"moz-extension://0c961d87-1e59-4481-b154-ca9358acab74/js/app.js\" type=\"text/javascript\"></script>"
+
+​
+
+`
 async function extractHTMLContent(url) {
   try {
     const response = await fetch(url);
@@ -18,8 +213,9 @@ async function extractHTMLContent(url) {
 
 function extractText(page) {
 
-  return page.replace(/<script[^>]*>.+?<\/script>/g, '').replace(/<.+?>/g, ' ');
+  return page.replace(/<script[^>]*>.+?<\/script>/gs, '').replace(/<.+?>/g, ' ');
 };
+console.log(extractText(page))
 extractHTMLContent(targetURL)
     .then((htmlContent) => {
       if (htmlContent) {
